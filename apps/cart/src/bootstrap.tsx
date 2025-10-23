0ad9f5495838ade0ed921671a8528985f825170d
timirevV <timirev.fortech@gmail.com>
2025-10-23 11:32:27 +0300

    feat: add sentry

--- apps/cart/src/bootstrap.tsx
+++ apps/cart/src/bootstrap.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import * as Sentry from "@sentry/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const rootEl = document.getElementById('root');
+Sentry.init({
+  dsn: "ваш_DSN",
+  sendDefaultPii: true,
+});
+
+const rootEl = document.getElementById("root");
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <App />
-    </React.StrictMode>,
+      <Sentry.ErrorBoundary fallback={<h2>Что-то пошло не так</h2>}>
+        <App />
+      </Sentry.ErrorBoundary>
+    </React.StrictMode>
   );
 }
